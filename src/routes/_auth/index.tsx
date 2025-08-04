1c4b0466b5e9c22e432f220fdbe861f2c540c783
sahimjbc <jbc.sahim@gmail.com>
2025-08-04 13:08:32 +0600

    feat: update dashboard titles and localization for chart display

--- src/routes/_auth/index.tsx
+++ src/routes/_auth/index.tsx
@@ -3,14 +3,12 @@ import { authAtom } from "@/store";
 import { createFileRoute } from "@tanstack/react-router";
 import { useAtom } from "jotai/react";
 
-import { TrendingUp } from "lucide-react";
 import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
 
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -74,8 +72,10 @@ export function RouteComponent() {
   return (
     <Card className="w-5xl mx-auto my-8">
       <CardHeader className="text-center">
-        <CardTitle>Line Chart - Multiple</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
+        <CardTitle>{m["dashboard.chart title"]()}</CardTitle>
+        <CardDescription>
+          {`${chartData[0].month} – ${chartData[chartData.length - 1].month} ${new Date().getFullYear()}`}
+        </CardDescription>
       </CardHeader>
       <CardContent>
         <ChartContainer config={chartConfig}>
@@ -132,18 +132,6 @@ export function RouteComponent() {
           </LineChart>
         </ChartContainer>
       </CardContent>
-      <CardFooter>
-        <div className="flex w-full items-start gap-2 text-sm">
-          <div className="grid gap-2">
-            <div className="flex items-center gap-2 leading-none font-medium">
-              Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-            </div>
-            <div className="text-muted-foreground flex items-center gap-2 leading-none">
-              Showing total visitors for the last 6 months
-            </div>
-          </div>
-        </div>
-      </CardFooter>
     </Card>
   );
 }
